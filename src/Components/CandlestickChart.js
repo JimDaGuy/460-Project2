e3920d283c6aafe32d2cd6a11d1fa7840fc782d7
James Digrazia <jtd2401@rit.edu>
2019-04-08 00:25:07 -0400

    Implement initial candlestick chart

--- src/Components/CandlestickChart.js
+++ src/Components/CandlestickChart.js
@@ -8,8 +8,11 @@ class CandlestickChart extends Component {
   componentDidMount() {
     d3.csv(data, d => {
       return {
-        name: d.name,
-        wins: parseInt(d.wins)
+        date: new Date(parseInt(d.year), parseInt(d.month)),
+        open: parseInt(d.open),
+        close: parseInt(d.close),
+        lower: parseInt(d.lower),
+        upper: parseInt(d.upper)
       };
     })
       .then(data => {
@@ -21,11 +24,12 @@ class CandlestickChart extends Component {
   }
 
   visualizeData(dataset) {
-    const svgWidth = 500;
-    const svgHeight = 500;
+    const svgWidth = 700;
+    const svgHeight = 600;
     const chartIndent = 50;
+    const lineWidthPercent = 0.2;
+    const barWidthPercent = 0.45;
 
-    dataset.sort((a, b) => a.wins - b.wins);
     console.dir(dataset);
 
     let svg = d3
@@ -36,40 +40,84 @@ class CandlestickChart extends Component {
 
     // Plotting data
     let xScale = d3
-      .scaleBand()
-      .domain(dataset.map(d => d.name))
-      .rangeRound([chartIndent, svgWidth - chartIndent])
-      .padding(0.15)
-      .align(0.1);
+      .scaleTime()
+      .domain(d3.extent(dataset, d => d.date))
+      .range([chartIndent, svgWidth - chartIndent]);
 
     let yScale = d3
       .scaleLinear()
-      .domain([0, d3.max(dataset, d => d.wins)])
+      .domain([0, d3.max(dataset, d => d.upper) + 5])
       .range([svgHeight - chartIndent, chartIndent]);
 
+    // Draw lower/upper line
     svg
-      .selectAll("rect")
-      .data(dataset, d => d.name)
+      .selectAll(".upperLower")
+      .data(dataset)
       .enter()
       .append("rect")
-      .attr("x", d => xScale(d.name))
-      .attr("y", d => yScale(d.wins))
-      .attr("width", xScale.bandwidth())
-      .attr("height", d => svgHeight - chartIndent - yScale(d.wins))
-      .attr("fill", "orange")
-      .attr("stroke", "black");
+      // X - Half of bar width
+      .attr("x", d => {
+        const initialXPos = xScale(d.date);
+        const lineWidth =
+          ((svgWidth - chartIndent * 2) / dataset.length) * lineWidthPercent;
+        return initialXPos - lineWidth / 2;
+      })
+      .attr("y", d => {
+        return yScale(d.upper);
+      })
+      .attr(
+        "width",
+        ((svgWidth - chartIndent * 2) / dataset.length) * lineWidthPercent
+      )
+      .attr("height", d => svgHeight - chartIndent - yScale(d.upper - d.lower))
+      .attr("fill", "gray")
+      .append("title")
+      .text(d => `Upper: ${d.upper} Lower: ${d.lower}`);
+
+    // Draw open/close line
+    svg
+      .selectAll(".openClose")
+      .data(dataset)
+      .enter()
+      .append("rect")
+      // X - Half of bar width
+      .attr("x", d => {
+        const initialXPos = xScale(d.date);
+        const lineWidth =
+          ((svgWidth - chartIndent * 2) / dataset.length) * barWidthPercent;
+        return initialXPos - lineWidth / 2;
+      })
+      .attr("y", d => {
+        return yScale(Math.max(d.open, d.close));
+      })
+      .attr(
+        "width",
+        ((svgWidth - chartIndent * 2) / dataset.length) * barWidthPercent
+      )
+      .attr(
+        "height",
+        d => svgHeight - chartIndent - yScale(Math.abs(d.open - d.close))
+      )
+      .attr("fill", d => (d.open > d.close ? "red" : "green"))
+      .append("title")
+      .text(d => `Open: ${d.open} Close: ${d.close}`);
 
     // Axes
     svg
       .append("g")
       .attr("class", CandlestickChartStyles.xAxis)
       .attr("transform", `translate(0, ${svgHeight - chartIndent})`)
-      .call(d3.axisBottom(xScale));
+      .call(
+        d3
+          .axisBottom(xScale)
+          .tickFormat(d3.timeFormat("%b"))
+          .ticks(dataset.length)
+      );
 
     svg
       .append("g")
       .attr("class", CandlestickChartStyles.yAxis)
-      .attr("transform", `translate(${chartIndent}, 0 )`)
+      .attr("transform", `translate(${chartIndent - 20}, 0 )`)
       .call(d3.axisLeft(yScale));
   }
 
